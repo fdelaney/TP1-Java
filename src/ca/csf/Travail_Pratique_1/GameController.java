a9936b2a0e711150090d78615ddda86ce7157ad7
fdelaney <delaneyfrancis@hotmail.com>
2014-09-23 10:26:02 -0400

    Update Benjamin

--- src/ca/csf/Travail_Pratique_1/GameController.java
+++ src/ca/csf/Travail_Pratique_1/GameController.java
@@ -8,10 +8,10 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class GameController {
-	Tower[] tower = { new Tower(new ArrayStack(3)), new Tower(new ArrayStack(3)),
-			new Tower(new ArrayStack(3)) };
+	Tower[] tower = { new Tower(new ArrayStack(3)),
+			new Tower(new ArrayStack(3)), new Tower(new ArrayStack(3)) };
+	private Disk currentRectangle;
 
-	private Disk currentRectangle = null;
 	@FXML
 	private Button Enl1;
 	@FXML
@@ -24,45 +24,111 @@ public class GameController {
 	private Button Dep2;
 	@FXML
 	private Button Dep3;
-	
+
 	@FXML
 	private VBox tour1Vbox;
 	@FXML
 	private VBox tour2Vbox;
 	@FXML
 	private VBox tour3Vbox;
+	@FXML
+	private VBox currentRec;
 
-	public void DeposerTour(ActionEvent actionEvent){
+	private void StartNewGame(){
 		
-		Enl1.setDisable(false);
-		Enl2.setDisable(false);
-		Enl3.setDisable(false);
+	}
+	
+	@FXML
+	public void VerificationTours() {
+
+	}
+
+	public void EnleverTour(ActionEvent actionEvent) {
+		// --- Disable buttons
+
+		Enl1.setDisable(true);
+		Enl2.setDisable(true);
+		Enl3.setDisable(true);
+
+		if (actionEvent.getSource() == Enl1) {
+			currentRectangle = tower[0].removeDisk();
+		} else if (actionEvent.getSource() == Enl2) {
+			currentRectangle = tower[1].removeDisk();
+		} else {
+			currentRectangle = tower[2].removeDisk();
+		}
+
+		// --- Disable buttons Si le disque au top de la tour doit être plus
+		// grande
+		if (currentRectangle.getSize() > tower[0].getDiskOnTop().getSize()) {
+			Dep1.setDisable(true);
+		} else {
+			Dep1.setDisable(false);
+		}
+		if (currentRectangle.getSize() > tower[1].getDiskOnTop().getSize()) {
+			Dep2.setDisable(true);
+		} else {
+			Dep2.setDisable(false);
+		}
+		if (currentRectangle.getSize() > tower[2].getDiskOnTop().getSize()) {
+			Dep3.setDisable(true);
+		} else {
+			Dep3.setDisable(false);
+		}
+		// --- End Disable Buttons
+
+		RefreshDisks();
+	}
+
+	public void DeposerTour(ActionEvent actionEvent) {
+
+		if (actionEvent.getSource() == Enl1) {
+			tower[0].addDisk(currentRectangle);
+		} else if (actionEvent.getSource() == Enl2) {
+			tower[1].addDisk(currentRectangle);
+		} else {
+			tower[2].addDisk(currentRectangle);
+		}
+
+		currentRectangle = null;
+		
+		if (tower[0].getSize() != 0) {
+			Enl1.setDisable(false);
+		}
+		if (tower[1].getSize() != 0) {
+			Enl2.setDisable(false);
+		}
+		if (tower[2].getSize() != 0) {
+			Enl3.setDisable(false);
+		}
 		Dep1.setDisable(true);
 		Dep2.setDisable(true);
 		Dep3.setDisable(true);
-		
-		if(actionEvent.getSource() == Dep1){
-			tower[0].addDisk(currentRectangle);
-		}
-		else if(actionEvent.getSource() == Dep2){
-			tower[1].addDisk(currentRectangle);
-		}
-		else if(actionEvent.getSource() == Dep3){
-			tower[2].addDisk(currentRectangle);
-		}
-		currentRectangle = null;
+
+
 		RefreshDisks();
 	}
-	
-	
-	
+
 	@FXML
 	public void RefreshDisks() {
+
+		tour1Vbox.getChildren().clear();
+		tour2Vbox.getChildren().clear();
+		tour3Vbox.getChildren().clear();
+
+		if (currentRectangle != null) {
+			Rectangle rectangleCurrent = new Rectangle(
+					currentRectangle.getSize(), 50);
+			rectangleCurrent = ColorOfRectangle(rectangleCurrent);
+			currentRec.getChildren().add(rectangleCurrent);
+		} else {
+			currentRec.getChildren().clear();
+		}
+
 		for (int i = 0; i < 3; i++) {
 			if (tower[i].getSize() > -1) {
 				for (int x = tower[i].getSize(); x > 0; x--) {
-					Rectangle rectangle = new Rectangle(tower[x].getDiskAt(x)
-							.getSize(), 50);
+					Rectangle rectangle = new Rectangle(tower[x].getDiskAt(x).getSize(), 50);
 					rectangle = ColorOfRectangle(rectangle);
 
 					if (i == 0) {
@@ -81,14 +147,14 @@ public class GameController {
 		String[] color = { "BLUE", "GREEN", "YELLOW", "ORANGE", "RED", "PURPLE" };
 
 		int width = 50;
-		boolean confirmation = false;
+
 		int NbOfTheColor = 0;
 
-		while (confirmation = false || NbOfTheColor > 5) {
+		while (NbOfTheColor > 5) {
 			if (rectangle.getWidth() == width) {
 
 				rectangle.setFill(Color.valueOf(color[NbOfTheColor]));
-				confirmation = true;
+				break;
 			}
 			width = width + 10;
 			NbOfTheColor++;
@@ -97,7 +163,3 @@ public class GameController {
 		return rectangle;
 	}
 }
-// il faut rentrer en propriété le numero de la tour
-// Une fonction pour créer tout les rectangles d'une tower
-// On va prendre la largueur du rectangle par le Disk de la classe Tower
-// Loop pour chaque rectangle
